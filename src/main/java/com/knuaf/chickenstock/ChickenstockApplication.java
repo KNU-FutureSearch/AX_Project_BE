373528320b69fb751bf9cc46cf58b5b41e2828cf
chanwoong <oco466kr@knu.ac.kr>
2026-04-26 16:37:42 +0900

    fix : main application 파일 중복을 수정해서 수정

--- src/main/java/com/knuaf/chickenstock/ChickenstockApplication.java
+++ src/main/java/com/knuaf/chickenstock/ChickenstockApplication.java
@@ -2,12 +2,13 @@ package com.knuaf.chickenstock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling; // 추가
 
 @SpringBootApplication
+@EnableScheduling // 💡 스케줄링 기능을 여기로 가져옵니다.
 public class ChickenstockApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChickenstockApplication.class, args);
 	}
-
 }
